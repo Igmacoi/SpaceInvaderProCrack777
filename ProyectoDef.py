65a3cd4affaaa2ea47c357e5ad73fda8eb5f96b0
Ignacio <yo.yo.yo.kkl@hotmail.com>
2022-06-20 18:13:29 -0400

    Falta agg menu

--- ProyectoDef.py
+++ ProyectoDef.py
@@ -1,4 +1,3 @@
-from platform import platform
 import pygame
 import random
 #---------------------------------------------------------------------
@@ -9,19 +8,17 @@ import random
 # mayor puntaje posible antes de que nuestra vida llege a 0
 #---------------------------------------------------------------------
 
-ScreenX,ScreenY = 500,700  #ventana
-
-Run = True
-
-
-#Se define el color par que sea trasparente 
-BLACK = (0, 0, 0)        
-CoInvi = (255, 174, 201) #Color salmon
-Blanco = (160,0,240)
-
+#Inicializamos Pygame
 pygame.init()
 pygame.mixer.init()
 
+ScreenX,ScreenY = 500,700  #ventana
+
+#para el While, True es para que se mantenga activo
+Run = True
+
+#Se define el color par que sea trasparente  
+CoInvi = (255, 174, 201) #Color salmon
 
 #crea la ventana
 screen = pygame.display.set_mode((ScreenX, ScreenY)) 
@@ -33,7 +30,8 @@ y = 0
 #Titulo de la ventana
 pygame.display.set_caption("Space Invader Pro crack 2.0")
 
-clock = pygame.time.Clock()
+#Ayuda a controlar el tiempo (FPS)
+clock = pygame.time.Clock()     
 
 #Sonido al disparar
 Sdisp = pygame.mixer.Sound('cosas/laser.wav')
@@ -42,35 +40,64 @@ SexplE = pygame.mixer.Sound('cosas/explosound.wav')
 
 #ponemos las imagenes en una lista
 explot = []
+#toma los valores del 1 al 9 de las imagenes
 for i in range(1,9):
+    #{i}, itera entre los nombres de las explociones del 1 al 9
     Explo = pygame.image.load(f'Explociones/{i}.png')
+    #habre la lista y guarda en ella lo que esta en Explo
     explot.append(Explo)
 
+#El score empieza en 0
 score = 0
-vida = 100
 
-
-def vida(frame, x,y, nivel):
-    largo = 100
-    alto = 20 
-    fill = int((nivel/100)*largo)
-    border = pygame.Rect(x,y, largo, alto)
-    fill = pygame.Rect(x,y,fill, alto)
-    pygame.draw.rect(frame, (255,0,55),fill)
-    pygame.draw.rect(frame, negro, border,4)
-
-
-window = pygame.display.set_mode((ScreenX, ScreenY))
+#colores para los textos
 blanco = (255,255,255)
-negro = (0,0,0)
-def puntuacion(frame, text, size, x,y):
-	font = pygame.font.SysFont('Small Fonts', size, bold=True)
-	text_frame = font.render(text, True, blanco,negro)
+negro  = (0,0,0)
+verde  = (34,177,76)
+
+
+#Entrada  frame  screen
+#         nivel  vida
+#         x      coordenada en x
+#         y      coordenada en y
+#Salida
+def vida(frame, x,y, nivel):
+    #(screen,  0, 0 ,player.vida)
+    largo = 100 
+    alto = 20 
+    #la cantidad de vida, 100 puntos de vida 
+    #fill es una variable que ira disminuyendo 
+    fill = int((nivel/100)*largo)
+    #Se crea un rectangulo en donde estara la vida
+    # x,y posicion 
+    border = pygame.Rect(x,y, largo, alto)
+    #creamos la barra de vida 
+    fill = pygame.Rect(x,y,fill, alto)
+    #Dibujamos la barra de vida
+    pygame.draw.rect(frame, verde ,fill)
+    #Dibujamos los bordes,                espesor 
+    pygame.draw.rect(frame, negro, border,5)
+
+#Entrada  frame  screen
+#         text   score
+#         size - tamano
+#         x      coordenada en x
+#         y      coordenada en y
+#Salida
+def puntuacion(frame, text,   size,      x,       y):
+#         (screen, str(score), 25, ScreenX // 2, 10)
+                                #Tipo de letra-- tamano --negrita(que sea mas notoria)
+	Fuente = pygame.font.SysFont('bahnschrift',  size,   bold=True) #creamos el tipo de letra
+                            #  texto       colores
+	text_frame = Fuente.render(text, True, blanco,negro)
+        #Para obtener las coordenadas donde se pondra la puntuacion
 	text_rect = text_frame.get_rect()
+    #Damos las coordenadas
 	text_rect.midtop = (x,y)
+    #Lugar en el que se pondra el texto, en este caso en la ventana (screen)
 	frame.blit(text_frame, text_rect)
 
-class Player(pygame.sprite.Sprite):
+class Jugador(pygame.sprite.Sprite):
     #entrada self -Es una instancia para llamar al objeto
     #salida  
     def __init__(self):
@@ -145,11 +172,9 @@ class Enemigos(pygame.sprite.Sprite):
         self.image = pygame.image.load('cosas/Enemi1.png').convert()#llamamos a la imagen
         self.image.set_colorkey(CoInvi)#ponemos el color trasparente
         self.rect = self.image.get_rect()
-        self.rect.x =  random.randrange(1, ScreenX - 50)#pocicion de donde sale la bala en eje
-                                                #x de forma aleatoria entre los parametros dados
-        self.rect.y = random.randrange(1, 400)#pocicion de donde sale la bala en eje
-                                         #y de forma aleatoria entre los parametros dados
-        self.speedy = -50 #velocidad de los enemigos en el eje Y, -50 es para donde se mueven (izquierda)
+        self.rect.x =  random.randrange(1, ScreenX - 50)                               
+        self.rect.y = random.randrange(10, 340)
+        #self.speedyE = -10 #velocidad de los enemigos en el eje Y, -50 es para donde se mueven (izquierda)
 
     #entrada self -Es una instancia para llamar al objeto
     #salida 
@@ -162,15 +187,17 @@ class Enemigos(pygame.sprite.Sprite):
             self.rect.x = 0
             self.rect.y += 50
 
+
     #entrada self -Es una instancia para llamar al objeto
     #salida 
     def disparo_enemigo(self):
         #pocicion de la bala (de donde sale)
         #llamamos a la clase BalasEnemigos
-        balaE = BalasEnemigos(self.rect.centerx, self.rect.top)
+        bala = BalasEnemigos(self.rect.centerx, self.rect.top)
         #agg bala a los sprites
-        Tsprites.add(balaE)  
-        balas.add(balaE)
+        Tsprites.add(bala)  
+        balas_enemigos.add(bala)
+        #poner sonido
 
 class BalasEnemigos(pygame.sprite.Sprite):
     #entrada self -Es una instancia para llamar al objeto
@@ -183,21 +210,21 @@ class BalasEnemigos(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.image, 180)#rotamos la imagen en 180 grados
         self.image.set_colorkey(CoInvi)#ponemos el color trasparente
         self.rect = self.image.get_rect()
-        self.rect.y = random.randrange(10, ScreenX)#pocicion de donde sale la bala en eje
+        self.rect.y = random.randrange(10, 300)#pocicion de donde sale la bala en eje
                                          #y de forma aleatoria entre los parametros dados
         
         self.rect.centerx = x #pocicion de donde sale la bala en eje X, en el centro de la imagen
                             #del enemigo que la disparo
-        self.speedy = 10 #velocidad de la nave en eje Y, pisitivo para que suba
+        self.speedx = 8 #velocidad de la bala en eje Y
 
     #entrada self -Es una instancia para llamar al objeto
     #salida 
     def update(self):
-        self.rect.y += self.speedy
-        #cuando llegen al limite (ScreenY)=700, inferior
-        #desaparece
+        self.rect.y += self.speedx
+        #cuando llegen al limite (ScreenY)=700, inferior desaparece
         if self.rect.bottom > 700:
             self.kill()
+
 
 class Explo(pygame.sprite.Sprite):
     #entrada self -Es una instancia para llamar al objeto
@@ -216,7 +243,6 @@ class Explo(pygame.sprite.Sprite):
     #Salida
     def update(self):
         tiempo = pygame.time.get_ticks() #Cuanto tiempo a trascurrudi desde la explicion 
-        #si han trascurrido mas de (vel_explot), NO SE COMO EXPLICARR XDDXDXDXDXDXDXDX
         if tiempo - self.time > self.Vel_Explot:
             #Aumenta en 1 los frames para ir variando en las imagenes
             self.time = tiempo 
@@ -240,17 +266,17 @@ balas = pygame.sprite.Group()
 enemigos = pygame.sprite.Group()
 balas_enemigos = pygame.sprite.Group()
 
-player = Player()
+player = Jugador()
 Tsprites.add(player)
+balas.add(player)
+
 
 #rango de aparicion de los enemigos
- #VER COMO FUNCIONA
-
 for x in range(20):
-    enemi = Enemigos(10,10) 
+    enemi = Enemigos(20,20) 
     enemigos.add(enemi)
     Tsprites.add(enemi)
- 
+  
 while Run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -281,9 +307,9 @@ while Run:
         #Disparo del enemigo 
         enemi.disparo_enemigo()
         #Cada vez que muera 1, aparece otro        
-        enemigoss = Enemigos(300,10) #ver para que es
-        enemigos.add(enemigoss)
-        Tsprites.add(enemigoss)
+        enemi = Enemigos(10,10)
+        enemigos.add(enemi)
+        Tsprites.add(enemi)
 
         bum = Explo(i.rect.center) 
         Tsprites.add(bum)
@@ -292,17 +318,25 @@ while Run:
 
     #colicion balas enemigos - jugador 
     colicionJ = pygame.sprite.spritecollide(player, balas_enemigos, True)
-    for a in colicionJ:
+    for j in colicionJ:
         player.vida -= 10
         if player.vida <= 0:
-            run = False 
-        explo1 = Explo(a.rect.center)
-        Tsprites.add(explo1)
-    
-    
+            Run = False 
+        bum = Explo(j.rect.center)
+        Tsprites.add(bum)
+        SexplE.play()
 
-    puntuacion(window, ('  SCORE: '+ str(score)+'       '), 30, ScreenX-85, 2)
-    vida(window, ScreenX-285, 0, player.vida)
+    #colicion jugador - enemigo
+    hits = pygame.sprite.spritecollide(player, enemigos , False)
+    for hit in hits:
+        player.vida -= 100 
+        if player.vida <=0:
+            explo2 = Explo(hit.rect.center)
+            Tsprites.add(explo2)
+            Run = False
+    
+    puntuacion(screen, str(score), 20, ScreenX // 2, 10)
+    vida(screen,0,0 ,player.vida)
 
 
     clock.tick(60)
